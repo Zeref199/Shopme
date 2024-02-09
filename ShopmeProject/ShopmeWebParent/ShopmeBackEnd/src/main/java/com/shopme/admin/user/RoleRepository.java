7752de1dabf70945492792ca3f4667498bd3da4c
Otmaneox <arsalane.otmane@gmail.com>
2024-02-09 20:10:47 +0100

    added update user funct, delete user, enableStatus, userPhotos, Pagination, SortingForUserList

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/RoleRepository.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/RoleRepository.java
@@ -1,8 +1,8 @@
 package com.shopme.admin.user;
 
+import com.shopme.common.entity.Role;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import com.shopme.common.entity.Role;
 
 
 @Repository
--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -7,11 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+@DataJpaTest(showSql = false)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Rollback(value = false)
 public class UserRepositoryTests {
@@ -90,4 +95,34 @@ public class UserRepositoryTests {
 
         assertThat(user).isNotNull();
     }
+    @Test
+    public void testCountById(){
+        Integer id = 1;
+        Long countById = repo.countById(id);
+
+        assertThat(countById).isNotNull().isGreaterThan(0);
+
+    }
+
+    @Test
+    public void testDisableUser(){
+        Integer id = 1;
+        repo.updateEnabledStatus(id, false);
+    }
+
+    @Test
+    public void testListFirstPage(){
+        int pageNumber = 1;
+        int pageSize = 4;
+
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        Page<User> page = repo.findAll(pageable);
+
+        List<User> listUsers =  page.getContent();
+
+        listUsers.forEach(user-> System.out.println(user));
+
+        assertThat(listUsers.size()).isEqualTo(pageSize);
+    }
+
 }

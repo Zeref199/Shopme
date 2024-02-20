ea494731432b1fc2dd338f334e51c1112d807973
Otmaneox <arsalane.otmane@gmail.com>
2024-02-20 17:33:43 +0100

    fixced 2 bugs one with the image in category had to be required the seconde one was the entityId not beeing displayed

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/target/classes/static/js/common_list.js
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/target/classes/static/js/common_list.js
@@ -3,7 +3,7 @@ function clearFilter(){
 }
 
 function showDeleteConfirmModal(link, entityName){
-    entityId = link.attr("entityId");
+    let entityId = link.attr("entityId");
 
     $("#yesButton").attr("href", link.attr("href"));
     $("#confirmText").text("Are you sure you want to delete this " + entityName + " ID " + entityId + " ?");

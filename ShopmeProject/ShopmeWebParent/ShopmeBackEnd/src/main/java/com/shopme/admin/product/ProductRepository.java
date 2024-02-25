1c0bb8bde7df99d63695dca27e8e462626bddf37
Otmaneox <arsalane.otmane@gmail.com>
2024-02-25 16:12:14 +0100

    added enable status functionnality

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
@@ -2,11 +2,18 @@ package com.shopme.admin.product;
 
 import com.shopme.common.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     public Product findByName(String name);
+
+    @Query("UPDATE Product p SET p.enabled = ?2 WHERE p.id = ?1")
+    @Modifying
+    public void updateEnabledStatus(Integer id, boolean enabled);
+
 
 }

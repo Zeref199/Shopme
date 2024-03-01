414abf5a0f6bfe683d65e89634e9db59dab1c614
Otmaneox <arsalane.otmane@gmail.com>
2024-03-01 12:33:13 +0100

    added Pagination, sort and filter

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.shopme.admin.product;
 
 import com.shopme.common.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -16,6 +18,13 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     @Query("UPDATE Product p SET p.enabled = ?2 WHERE p.id = ?1")
     @Modifying
     public void updateEnabledStatus(Integer id, boolean enabled);
+
+    @Query("SELECT p FROM Product p WHERE p.name LIKE %?1%"
+                + "OR p.shortDescription LIKE %?1%"
+                + "OR p.fullDescription LIKE %?1%"
+                + "OR p.brand.name LIKE %?1%"
+                + "OR p.category.name LIKE %?1% ")
+    public Page<Product> findAll(String keyword, Pageable pageable);
 
 
 }

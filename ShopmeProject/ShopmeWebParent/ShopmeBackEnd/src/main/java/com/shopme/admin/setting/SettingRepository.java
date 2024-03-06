a3f1e6fbc71da4541eac3d3a6501dde3b38368b8
Otmaneox <arsalane.otmane@gmail.com>
2024-03-06 15:19:45 +0100

    added Setting entity, Repository and unit tests

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/SettingRepository.java
@@ -0,0 +1,9 @@
+package com.shopme.admin.setting;
+
+import com.shopme.common.entity.Setting;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SettingRepository extends JpaRepository<Setting, String> {
+}

97b9a6e19c6a671187174dda7f41b59030c6cb6a
Otmaneox <arsalane.otmane@gmail.com>
2024-03-12 14:28:21 +0000

    added customer Registration Form

---
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/setting/StateRepository.java
@@ -0,0 +1,14 @@
+package com.shopme.setting;
+
+import com.shopme.common.entity.Country;
+import com.shopme.common.entity.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StateRepository extends JpaRepository<State, Integer> {
+
+    public List<State> findByCountryOrderByNameAsc(Country country);
+}
--- ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/setting/StateRestController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/setting/StateRestController.java
@@ -1,10 +1,12 @@
-package com.shopme.admin.setting.state;
+package com.shopme.setting;
 
 import com.shopme.common.entity.Country;
 import com.shopme.common.entity.State;
 import com.shopme.common.entity.StateDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +16,7 @@ public class StateRestController {
     @Autowired
     private StateRepository repo;
 
-    @GetMapping("/states/list_by_country/{id}")
+    @GetMapping("/settings/list_states_by_country/{id}")
     public List<StateDTO> listByCountry(@PathVariable("id") Integer countryId){
         List<State> listStates = repo.findByCountryOrderByNameAsc(new Country(countryId));
         List<StateDTO> result = new ArrayList<>();
@@ -23,16 +25,5 @@ public class StateRestController {
             result.add(new StateDTO(state.getId(), state.getName()));
         }
         return result;
-    }
-
-    @PostMapping("/states/save")
-    public String save(@RequestBody State state){
-        State savedState = repo.save(state);
-        return String.valueOf(savedState.getId());
-    }
-
-    @DeleteMapping("/states/delete/{id}")
-    public void delete(@PathVariable("id") Integer id){
-        repo.deleteById(id);
     }
 }

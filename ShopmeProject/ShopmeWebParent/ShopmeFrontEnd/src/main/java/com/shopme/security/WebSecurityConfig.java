d9109234fa1c2177786c05b3dbba9a6f4ed5cf67
Otmaneox <arsalane.otmane@gmail.com>
2024-03-20 14:37:42 +0000

    Coded add Product to the shopping Cart

--- ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/security/WebSecurityConfig.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/security/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -70,7 +71,9 @@ public class WebSecurityConfig {
                         .permitAll())
                 .rememberMe((remember) -> remember
                         .key("1234567890_aBcDeFgHiJkLmNoPqRsTuVwXyZ")
-                        .tokenValiditySeconds(14 * 24 * 60 * 60));
+                        .tokenValiditySeconds(14 * 24 * 60 * 60))
+                .sessionManagement((sessionManagement) -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
 
         return http.build();
     }

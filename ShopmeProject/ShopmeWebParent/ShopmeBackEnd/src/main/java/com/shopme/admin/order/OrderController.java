8adb921e001ed5232cb28d03e7f6584bb9dae97d
Otmaneox <arsalane.otmane@gmail.com>
2024-03-23 00:37:06 +0000

    Coded View Order Details

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderController.java
@@ -2,19 +2,25 @@ package com.shopme.admin.order;
 
 import com.shopme.admin.paging.PagingAndSortingHelper;
 import com.shopme.admin.paging.PagingAndSortingParam;
+import com.shopme.admin.security.ShopmeUserDetails;
 import com.shopme.admin.setting.SettingService;
 import com.shopme.common.entity.Setting;
+import com.shopme.common.entity.order.Order;
+import com.shopme.common.exception.OrderNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
 public class OrderController {
-    private String defaultRedirectURL = "redirect:/orders/page/1?sortField=orderTime&sortDir=desc";
+    private final String defaultRedirectURL = "redirect:/orders/page/1?sortField=orderTime&sortDir=desc";
 
     @Autowired
     private OrderService orderService;
@@ -29,10 +35,14 @@ public class OrderController {
     public String listByPage(
             @PagingAndSortingParam(listName = "listOrders", moduleURL = "/orders") PagingAndSortingHelper helper,
             @PathVariable(name = "pageNum") int pageNum,
-            HttpServletRequest request) {
+            HttpServletRequest request, @AuthenticationPrincipal ShopmeUserDetails loggedUser) {
 
         orderService.listByPage(pageNum, helper);
         loadCurrencySetting(request);
+
+        if (!loggedUser.hasRole("Admin") && !loggedUser.hasRole("Salesperson") && loggedUser.hasRole("Shipper")) {
+            return "orders/orders_shipper";
+        }
 
         return "orders/orders";
     }
@@ -43,5 +53,30 @@ public class OrderController {
         for (Setting setting : currencySettings) {
             request.setAttribute(setting.getKey(), setting.getValue());
         }
+    }
+
+    @GetMapping("/orders/detail/{id}")
+    public String viewOrderDetails(@PathVariable("id") Integer id, Model model,
+                                   RedirectAttributes ra, HttpServletRequest request,
+                                   @AuthenticationPrincipal ShopmeUserDetails loggedUser) {
+        try {
+            Order order = orderService.get(id);
+            loadCurrencySetting(request);
+
+            boolean isVisibleForAdminOrSalesperson = false;
+
+            if (loggedUser.hasRole("Admin") || loggedUser.hasRole("Salesperson")) {
+                isVisibleForAdminOrSalesperson = true;
+            }
+
+            model.addAttribute("isVisibleForAdminOrSalesperson", isVisibleForAdminOrSalesperson);
+            model.addAttribute("order", order);
+
+            return "orders/order_details_modal";
+        } catch (OrderNotFoundException ex) {
+            ra.addFlashAttribute("message", ex.getMessage());
+            return defaultRedirectURL;
+        }
+
     }
 }

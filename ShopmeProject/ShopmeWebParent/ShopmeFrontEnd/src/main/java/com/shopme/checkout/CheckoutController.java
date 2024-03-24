bb40e07b6daa24c5932ae0a1fe58259d0ae8f1d9
Otmaneox <arsalane.otmane@gmail.com>
2024-03-24 16:02:07 +0000

    Coded Place Order with COD

--- ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/CheckoutController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/CheckoutController.java
@@ -6,14 +6,21 @@ import com.shopme.common.entity.Address;
 import com.shopme.common.entity.CartItem;
 import com.shopme.common.entity.Customer;
 import com.shopme.common.entity.ShippingRate;
+import com.shopme.common.entity.order.Order;
+import com.shopme.common.entity.order.PaymentMethod;
+import com.shopme.order.OrderService;
+import com.shopme.setting.SettingService;
 import com.shopme.shipping.ShippingRateService;
 import com.shopme.shoppingcart.ShoppingCartService;
+import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 @Controller
@@ -25,6 +32,8 @@ public class CheckoutController {
     @Autowired private AddressService addressService;
     @Autowired private ShippingRateService shipService;
     @Autowired private ShoppingCartService cartService;
+    @Autowired private OrderService orderService;
+    @Autowired private SettingService settingService;
 
     @GetMapping("/checkout")
     public String showCheckoutPage(Model model, HttpServletRequest request) {
@@ -59,4 +68,32 @@ public class CheckoutController {
 
         return "checkout/checkout";
     }
+
+    @PostMapping("/place_order")
+    public String placeOrder(HttpServletRequest request)
+            throws UnsupportedEncodingException, MessagingException {
+        String paymentType = request.getParameter("paymentMethod");
+        PaymentMethod paymentMethod = PaymentMethod.valueOf(paymentType);
+
+        Customer customer = controllerHelper.getAuthenticatedCustomer(request);
+
+        Address defaultAddress = addressService.getDefaultAddress(customer);
+        ShippingRate shippingRate = null;
+
+        if (defaultAddress != null) {
+            shippingRate = shipService.getShippingRateForAddress(defaultAddress);
+        } else {
+            shippingRate = shipService.getShippingRateForCustomer(customer);
+        }
+
+        List<CartItem> cartItems = cartService.listCartItems(customer);
+        CheckoutInfo checkoutInfo = checkoutService.prepareCheckout(cartItems, shippingRate);
+
+        Order createdOrder = orderService.createOrder(customer, defaultAddress, cartItems, paymentMethod, checkoutInfo);
+        cartService.deleteByCustomer(customer);
+        //sendOrderConfirmationEmail(request, createdOrder);
+
+        return "checkout/order_completed";
+    }
+
 }
---
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/order/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.shopme.order;
+
+import com.shopme.common.entity.order.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+}

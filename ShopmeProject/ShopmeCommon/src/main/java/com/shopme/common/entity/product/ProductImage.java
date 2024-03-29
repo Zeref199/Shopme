c94c44292da30acef14ac2d06e5ec4ecd087916d
Otmaneox <arsalane.otmane@gmail.com>
2024-03-29 16:33:02 +0000

    moved Existing image Files to Amazon S3 and updated the code to get them with new url

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/product/ProductImage.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/product/ProductImage.java
@@ -1,5 +1,6 @@
 package com.shopme.common.entity.product;
 
+import com.shopme.common.Constants;
 import com.shopme.common.entity.IdBasedEntity;
 import jakarta.persistence.*;
 
@@ -43,7 +44,7 @@ public class ProductImage extends IdBasedEntity {
     }
 
     public String getImagePath() {
-        return "/product-images/" + product.getId() + "/extras" + "/" + this.name;
+        return Constants.S3_BASE_URI +  "/product-images/" + product.getId() + "/extras" + "/" + this.name;
     }
 
 

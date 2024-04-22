9d41d128a7b1e2fbf241b9391857721fb5518019
Otmaneox <arsalane.otmane@gmail.com>
2024-04-22 13:05:30 +0100

    Coded the Admin Dashboard

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
@@ -1,17 +1,26 @@
 package com.shopme.admin;
 
+import com.shopme.admin.dashboard.DashboardInfo;
+import com.shopme.admin.dashboard.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    private DashboardService dashboardService;
+
     @GetMapping("")
-    public String ViewHomePage(){
-        return "index";
+        public String viewHomePage(Model model) {
+            DashboardInfo summary = dashboardService.loadSummary();
+            model.addAttribute("summary", summary);
+            return "index";
     }
 
     @GetMapping("/login")
